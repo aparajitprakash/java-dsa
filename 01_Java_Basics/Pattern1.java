3de75b1241ce89a1d0da185b07dfa1cbf6ad908a
Aparajit Prakash <aparajitprakash5@gmail.com>
2026-09-14 15:27:14 +0530

    JAVA DSA

---
+++ 01_Java_Basics/Pattern1.java
@@ -0,0 +1,26 @@
+// 13-Sep-2026
+import java.util.Scanner;
+public class Pattern1 {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of rows: ");
+        int n = sc.nextInt();
+        // int m = sc.nextInt();
+
+        for(int i = 0; i < n; i++){
+
+            for(int j = 0; j<=i; j++){
+
+                System.out.print("* ");
+
+            }
+
+            System.out.println();
+
+        }
+        sc.close();
+    }
+
+}
